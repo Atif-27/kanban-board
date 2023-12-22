b0d3f17f9b4588d01a82eab150b0f5dbf1b8a727
Atif Ali <atif276ali@gmail.com>
2023-12-22 19:30:18 +0530

    FIxed user availability

--- components/UserIcon.js
+++ components/UserIcon.js
@@ -8,7 +8,7 @@ const UserIcon = ({ user }) => {
       >
         {user.name[0]}
         <span
-          className={`absolute -right-1 -bottom-1 bg-red-100 p-1 rounded-full border border-gray-500 ${
+          className={`absolute -right-1 -bottom-1  p-1 rounded-full border border-gray-500 ${
             user.available ? " bg-green-500" : " bg-yellow-500"
           }`}
         ></span>
